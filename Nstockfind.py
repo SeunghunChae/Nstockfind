89a3c0f3281e8e4484fcf9429e9bad1007ab51eb
gogocb1 <gogocb1@naver.com>
2023-06-13 16:56:16 +0900

    18번 무한루프 제거

--- Nstockfind.py
+++ Nstockfind.py
@@ -54,7 +54,7 @@ driver = webdriver.Chrome(service=service, options=options)
 
 url='https://m.stock.naver.com/search'
 
-for i in range(10):
+for i in range(18,19):
     print(i)
     content=''
     name=''
@@ -101,6 +101,8 @@ for i in range(10):
             #기업개요 내용 뜰때까지 기다림
             while True:
                 if is_element_present(driver, By.CSS_SELECTOR, '#content > div.OverviewContainer_overviewContainer__2Gzn5 > div.OverviewContainer_infoCorp__3K5qX'):
+                    break
+                elif is_element_present(driver, By.CSS_SELECTOR, '#content > div.OverviewContainer_overviewContainer__2Gzn5 > div.NewError_article__2j3to > strong'):
                     break
             comp=driver.find_element(By.CSS_SELECTOR,"#content > div.OverviewContainer_overviewContainer__2Gzn5 > div.OverviewContainer_infoCorp__3K5qX")
             content=comp.text.replace('\n','').replace(',','')
